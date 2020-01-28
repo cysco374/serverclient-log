0b6379ebc9b25f842f2219c008af80c077651508
cysco374 <cysco374@yahoo.co.id>
2020-01-29 01:58:05 +0700

    change mysql module

--- Reporter.py
+++ Reporter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import mysql.connector
+import MySQLdb
 import sys
 import Server
 
--- Server.py
+++ Server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import mysql.connector
+import MySQLdb
 import socket
 import sys
 import select
